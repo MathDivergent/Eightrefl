23f13ea6354e9c935b832e4d40e926f49a6c436d
MathDivergent <71389444+MathDivergent@users.noreply.github.com>
2025-07-22 02:27:50 +0300

    Upload Eightrefl v2.1.0.

---
+++ include/Eightrefl/Parent.hpp
@@ -0,0 +1,57 @@
+#ifndef EIGHTREFL_PARENT_HPP
+#define EIGHTREFL_PARENT_HPP
+
+#include <string> // string
+#include <any> // any
+#include <functional> // function
+
+#include <Eightrefl/Attribute.hpp>
+
+#include <Eightrefl/Detail/Meta.hpp>
+
+// .parent<R, reflectable_type>()
+#define PARENT(... /*reflectable_type*/) \
+    { \
+        auto xxparent = eightrefl::find_or_add_parent<CleanR, __VA_ARGS__>(xxreflection); \
+        injection.template parent<CleanR, __VA_ARGS__>(*xxparent); \
+        xxmeta = &xxparent->meta; \
+    }
+
+namespace eightrefl
+{
+
+struct type_t;
+struct meta_t;
+
+struct parent_t
+{
+    std::string const name;
+    type_t* const type = nullptr;
+    std::function<std::any(std::any const& child_context)> const cast = nullptr;
+    attribute_t<meta_t> meta;
+};
+
+template <typename ReflectableType, typename ParentReflectableType>
+auto handler_parent_cast()
+{
+    return [](std::any const& child_context) -> std::any
+    {
+        auto xxchild_pointer = std::any_cast<ReflectableType*>(child_context);
+        if constexpr (meta::is_static_castable<ParentReflectableType*, ReflectableType*>::value)
+        {
+            return static_cast<ParentReflectableType*>(xxchild_pointer);
+        }
+        else
+        {
+            #ifdef EIGHTREFL_RTTI_ENABLE
+            return dynamic_cast<ParentReflectableType*>(xxchild_pointer);
+            #else
+            return nullptr;
+            #endif // EIGHTREFL_RTTI_ENABLE
+        }
+    };
+}
+
+} // namespace eightrefl
+
+#endif // EIGHTREFL_PARENT_HPP
---
+++ test/TestAny.cpp
@@ -0,0 +1,34 @@
+#include <EightreflTestingBase.hpp>
+
+#include <Eightrefl/Standard/any.hpp>
+
+TEST(TestBuiltin, TestAny)
+{
+    auto type = eightrefl::standard()->find("std::any");
+
+    ASSERT("type", type != nullptr);
+    EXPECT("type-name", type->name == "std::any");
+    EXPECT("type-size", type->size == sizeof(std::any));
+    EXPECT("type-context", type->context != nullptr);
+
+    auto reflection = type->reflection;
+
+    ASSERT("reflection", reflection != nullptr);
+    EXPECT("reflection-name", reflection->name == "std::any");
+
+    EXPECT("factory-R()", reflection->factory.find("std::any()") != nullptr);
+    EXPECT("factory-R(R const&)", reflection->factory.find("std::any(std::any const&)") != nullptr);
+
+    EXPECT("function-reset", reflection->function.find("reset") != nullptr);
+    EXPECT("function-operator=", reflection->function.find("operator=") != nullptr);
+
+    #ifdef EIGHTREFL_FULLY_ENABLE
+    EXPECT("function-swap", reflection->function.find("swap") != nullptr);
+    #endif // EIGHTREFL_FULLY_ENABLE
+
+    EXPECT("function-has_value", reflection->function.find("has_value") != nullptr);
+
+    #ifdef EIGHTREFL_RTTI_ENABLE
+    EXPECT("function-type", reflection->function.find("type") != nullptr);
+    #endif // EIGHTREFL_RTTI_ENABLE
+}
